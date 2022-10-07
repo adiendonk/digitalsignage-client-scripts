d1866569293e9163682b75cda2f6c7f0284ff139
Adhi Indi Arysanto <adie_ndonk@yahoo.co.id>
2022-10-07 19:48:31 +0700

    adding command to disable: - screen saver - screen blanking - screen turn off

--- armbian/setup.sh
+++ armbian/setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Install Chromium
-sudo apt -y install chromium
+# Install Firefox-ESR
+sudo apt -y install firefox-esr
+
+# Turn off screensaver
+xset s off
+
+# Turn Off Blanking
+xset s noblank
+
+# Disable power management using dpms to power the monitor down
+xset s off -dpms
