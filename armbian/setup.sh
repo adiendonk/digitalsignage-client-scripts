71bcad9b23a247ea3cdf3e185b25d98e0d57d0eb
Adhi Indi Arysanto <adie_ndonk@yahoo.co.id>
2022-10-13 16:47:10 +0700

    fixing

--- armbian/setup.sh
+++ armbian/setup.sh
@@ -8,7 +8,7 @@ sudo chmod +x files/startup.sh
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "@reboot sh $HOME/digitalsignage-client-scripts/files/startup.sh" >> mycron
+echo "@reboot ihsanlab sh $HOME/digitalsignage-client-scripts/files/startup.sh" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
