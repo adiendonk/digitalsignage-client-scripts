7e7e75a61e7fa2b6900b733d180bd6ba8ba20bfc
Adhi Indi Arysanto <adie_ndonk@yahoo.co.id>
2022-10-13 15:45:42 +0700

    add startup scripts

--- armbian/setup.sh
+++ armbian/setup.sh
@@ -3,11 +3,6 @@
 # Install Firefox-ESR
 sudo apt -y install firefox-esr
 
-# Turn off screensaver
-xset s off
+sudo cp files/rc.local /etc/rc.local
 
-# Turn Off Blanking
-xset s noblank
-
-# Disable power management using dpms to power the monitor down
-xset s off -dpms
+sudo chmod +x /etc/rc.local
