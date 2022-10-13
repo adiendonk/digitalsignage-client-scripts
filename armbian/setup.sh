52580476ecacee328d7e937f5334ae648126934f
Adhi Indi Arysanto <adie_ndonk@yahoo.co.id>
2022-10-13 18:57:54 +0700

    fixing

--- armbian/setup.sh
+++ armbian/setup.sh
@@ -7,6 +7,13 @@ sudo cp files/startihsign.desktop ~/.config/autostart/
 
 sudo chmod +x files/startup.sh
 
+# Add Display Orientation Setting Helper File
 sudo cp files/DisplayOrientation.sh /home/ihsanlab/Desktop/
 
 sudo chmod +x /home/ihsanlab/Desktop/DisplayOrientation.sh
+
+
+# Add Network Setting Helper File
+sudo cp files/NetworkSetting.sh /home/ihsanlab/Desktop/
+
+sudo chmod +x /home/ihsanlab/Desktop/NetworkSetting.sh
